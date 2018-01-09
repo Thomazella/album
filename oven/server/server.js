095c7bd0db7782490c7537c989145deacbea9d22
Raphael Thomazella <1357rtr@gmail.com>
2018-01-09 02:16:34 -0200

    it serves most file types.

--- oven/server/server.js
+++ oven/server/server.js
@@ -1,41 +1,94 @@
 let http = require('http');
 let fs = require('fs');
-http.createServer((req,res) => {
-  console.group("⭐️  Request in:");
-  console.dir({
-    "method" : req.method,
-    "url" : req.url,
-    "user-agent" : req.headers["user-agent"],
+let path = require('path')
+let port = 8077
+let c = {
+  'green'   : '\033[32m',
+  'red'     : '\033[31m',
+  'yellow'  : '\033[33m',
+  'w'       : '\033[39m', //white
+}
+let debug = false
+http.createServer((request,response) => {
+  console.groupEnd(); //this req's console.group will be closed by the next req
+  debug && console.group(`⭐️  ${c.green}Request:${c.w}`);
+  debug && console.dir({
+    "method" : request.method,
+    "url" : request.url,
+    // "user-agent" : request.headers["user-agent"],
   });
-  console.groupEnd();
-  let file = `${process.cwd()}${req.url}`
-  if (req.url === "/") file = process.cwd()
-  fs.readFile(file, (err, data) =>{
+  let file = `${process.cwd()}${request.url}` //make absolute path
+  if (request.url === "/") file = process.cwd() //we avoid a case where file ends in "/"
+  let headers = {'Content-Type': 'text/html'}
+  let mimeTypes = {
+    '.html' :   'text/html',
+    '.js'   :   'text/javascript',
+    '.css'  :   'text/css',
+    '.sass' :   'text/css',
+    '.scss' :   'text/css',
+    '.map'  :   'text/css', //css source map to allow browser edit sass
+    '.json' :   'application/json',
+    '.txt'  :   'text/plain',
+    '.png'  :   'image/png',
+    '.jpg'  :   'image/jpeg',
+    '.gif'  :   'image/gif',
+    '.svg'  :   'image/svg+xml',
+    '.wav'  :   'audio/wav',
+    '.ogg'  :   'audio/ogg',
+    '.mp3'  :   'audio/mpeg',
+    '.midi' :   'audio/midi',
+    '.webm' :   'video/webm',
+    '.mp4'  :   'video/mp4',
+    '.pdf'  :   'application/pdf',
+    '.woff' :   'application/font-woff',
+    '.ttf'  :   'application/font-ttf',
+    '.eot'  :   'application/vnd.ms-fontobject',
+    '.otf'  :   'application/font-otf',
+    '.rar'  :   'application/x-rar-compressed',
+  };
+  fs.readFile(file, (err, data) => {
     if (err) {
       switch (err.code) {
         case 'ENOENT':
-          console.error(`${file} does not exist`);
-          //404
+          console.error(`${c.red} 🔥 ${file} does not exist, serving 404${c.w} 🔥`);
+          response.writeHead(404, headers)
+          response.write(`
+            <body style="font-family: arial">
+              <h1>404</h1>
+              <p>${request.headers.host}${request.url} was not found</p>
+              <a href="http://${request.headers.host}">go home</a>
+            </body>`)
+          response.end()
           return;
-          break;
         case 'EISDIR':
-          file = `${file}/index.html`
-          console.log(`dir request: "${req.url}", serving "${file}"`);
+          file = `${file}/index.html` //browser asked for a dir, give the index.html of that dir
+          console.log(`${c.yellow}dir request: "${request.url}", serving "${file}${c.w}"`);
           break;
         default:
-          console.log(err);
           throw err;
           return
       }
     }
+    let extention = String(path.extname(file)).toLowerCase()
+    headers['Content-Type'] = mimeTypes[extention] || 'application/octet-stream' //<= unknown type
     fs.readFile(file, (err, data) => {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data)
-      res.end();
+      if (err) {
+        console.dir(err)
+        response.writeHead(500, {'Content-Type':'text/html'})
+        response.write(`
+          <body style="font-family: arial">
+          <h1>500</h1>
+          <p>Internal error, we're sorry</p>
+          <a href="http://${request.headers.host}">go home</a>
+          </body>`)
+          response.end()
+          return;
+      }
+      debug && console.dir(headers)
+      response.writeHead(200, headers);
+      response.write(data)
+      response.end();
     })
   })
-}).listen(8077)
-let currentDir = process.cwd()
-// console.log(currentDir);
-// fs.readdir(currentDir,(err, files) => console.log(files))
-// process.exit(0)
+}).listen(port)
+console.log(`Running on http://localhost:${port}/`);
