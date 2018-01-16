df7c7fcc47b9a3c9389292f6a73e8ebe29947a66
Raphael Thomazella <1357rtr@gmail.com>
2018-01-16 16:06:33 -0200

    css: improvements. html: client-side form. server: started handling post requests

--- oven/server/server.js
+++ oven/server/server.js
@@ -1,21 +1,24 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path')
+const util = require('util')
+const url = require('url')
 const port = 8077
+const debug = true
 const c = {
   'green'   : '\033[32m',
   'red'     : '\033[31m',
   'yellow'  : '\033[33m',
   'white'   : '\033[39m',
 }
-const debug = true
 http.createServer((request,response) => {
   if (debug) {
     console.groupEnd(); //this req's console.group will be closed by the next req
     console.group(`⭐️  ${c.green}Request:${c.white}`);
     console.dir({"method" : request.method,"url" : request.url})
   }
-  let file = `${process.cwd()}${request.url}` //make absolute path
+  let uri = url.parse(request.url)
+  let file = `${process.cwd()}${uri.pathname}` //make absolute path
   if (request.url === "/") file = process.cwd() //we avoid a case where file ends in "/"
   let headers = {}
   const mimeTypes = {
@@ -44,55 +47,72 @@ http.createServer((request,response) => {
     '.otf'  :   'application/font-otf',
     '.rar'  :   'application/x-rar-compressed',
   };
-  fs.readFile(file, (err, data) => {
-    if (err) {
-      switch (err.code) {
-        case 'ENOENT':
-          console.error(`${c.red} 🔥 ${file} does not exist, serving 404${c.white} 🔥`);
-          response.writeHead(404, {'Content-Type': 'text/html'})
-          response.write(`
-            <head><meta charset="UTF-8"><link href="css/index.css" rel="stylesheet"></head>
-            <body class="errorPage">
-              <h1>404</h1>
-              <p class="url">${request.headers.host}${request.url}</p>
-              <span class="message">not found</span>
-              <span class="message"> 🤷🏻‍  </span>
-              <p class="link"><span> 👉🏻 </span>
-              <a href="http://${request.headers.host}">go home</a></p>
-            </body>`)
-          response.end()
-          return;
-        case 'EISDIR':
-          file = `${file}/index.html` //browser asked for a dir, give the index.html of that dir
-          console.log(`${c.yellow}dir request: "${request.url}", serving "${file}${c.white}"`);
-          break;
-        default:
-          throw err;
-          return
-      }
-    }
-    const extention = String(path.extname(file)).toLowerCase()
-    headers['Content-Type'] = mimeTypes[extention] || 'application/octet-stream' //<= unknown type
-    fs.readFile(file, (err, data) => {
-      if (err) {
-        console.dir(err)
-        response.writeHead(500, {'Content-Type':'text/html'})
-        response.write(`
-          <head><meta charset="UTF-8"><link href="css/index.css" rel="stylesheet"></head>
-          <body class="errorPage">
-            <h1>500</h1>
-            <p>Internal error, we're sorry</p>
-            <span> 👉🏻 </span>
-            <a href="http://${request.headers.host}">go home</a>
-          </body>`)
-          response.end()
-          return;
-      }
-      if (debug) console.dir(headers)
-      response.writeHead(200, headers);
-      response.write(data)
-      response.end();
-    })
-  })
+  switch (request.method) {
+    case 'POST':
+      console.dir(request.headers);
+      response.end()
+      break;
+    case 'GET':
+      fs.readFile(file, (err, data) => {
+        if (err) {
+          switch (err.code) {
+            case 'ENOENT':
+              console.error(`${c.red} 🔥 ${file} does not exist, serving 404${c.white} 🔥`);
+              response.writeHead(404, {'Content-Type': 'text/html'})
+              response.write(`
+                <head>
+                  <meta charset="UTF-8">
+                  <link href="css/index.css" rel="stylesheet">
+                  <title>Error 404</title>
+                </head>
+                <body class="errorPage">
+                  <h1>404</h1>
+                  <p class="url">${request.headers.host}${request.url}</p>
+                  <span class="message">not found</span>
+                  <p class="link"><a href="http://${request.headers.host}">go home</a></p>
+                </body>`)
+              response.end()
+              return;
+            case 'EISDIR':
+              file = `${file}/index.html` //browser asked for a dir, give the index.html of that dir
+              console.log(`${c.yellow}dir request: "${request.url}", serving "${file}${c.white}"`);
+              break;
+            default:
+              throw err;
+              return
+          }
+        }
+        const extention = String(path.extname(file)).toLowerCase()
+        headers['Content-Type'] = mimeTypes[extention] || 'application/octet-stream' //<= unknown type
+        fs.readFile(file, (err, data) => {
+          if (err) {
+            console.dir(err)
+            response.writeHead(500, {'Content-Type':'text/html'})
+            response.write(`
+              <head>
+              <meta charset="UTF-8">
+              <link href="css/index.css" rel="stylesheet">
+              <title>Error 500</title>
+              </head>
+              <body class="errorPage">
+                <h1>500</h1>
+                <p class="url">${request.headers.host}${request.url}</p>
+                <span class="message">Our bad. We're sorry.</span>
+                <p class="link"><a href="http://${request.headers.host}">go home</a></p>
+              </body>`)
+              response.end()
+              return;
+          }
+          if (debug) console.dir(headers)
+          response.writeHead(200, headers);
+          response.write(data)
+          response.end();
+        })
+      })
+      break;
+    default:
+      throw `Server doesn't know how to handle ${request.method} requests.`
+      return
+  }
 }).listen(port)
 console.log(`Running on http://localhost:${port}/`);
